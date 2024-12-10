3b654c48d30984e2cd5e4f51f5696bf443ee2066
HariHaran <101501297+hariharan138@users.noreply.github.com>
2024-12-10 18:44:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ document.getElementById("data-form").addEventListener("submit", async (e) => {
     const text = document.getElementById("text-input").value;
 
     try {
-        const response = await fetch("http://localhost:4000/data", {
+        const response = await fetch("https://sharetext-backend.onrender.com/data", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ text }),
         });
         const result = await response.json();
         // alert(`${result.id}`);
-        document.write(`http://localhost:4000/${result.id}`)
+        document.write(`https://sharetext-backend.onrender.com/${result.id}`)
     } catch (error) {
         console.error("Error submitting data:", error);
     }
